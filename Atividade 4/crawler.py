f8dabcf6b9fcf469fd807b63214c1cb1d6909c9b
lithiago <thiagoramon993@gmail.com>
2026-03-24 16:40:29 -0300

    update

--- Atividade 4/crawler.py
+++ Atividade 4/crawler.py
@@ -1,10 +1,9 @@
-import requests
+import urllib.request
 from bs4 import BeautifulSoup
 
 
 with open ("./seeds.txt", "r", encoding="UTF-8") as file:
-    links = []
-    for line in file:
-        links.append(line)
+    links = file.read()
         
-print(links)
+
+page = urllib.request.urlopen("")    
