12173c176b73c38c44d650b7913f3b6b2a2ce584
lithiago <thiagoramon993@gmail.com>
2026-03-24 22:58:31 -0300

    update

--- Atividade 4/crawler.py
+++ Atividade 4/crawler.py
@@ -3,7 +3,37 @@ from bs4 import BeautifulSoup
 
 
 with open ("./seeds.txt", "r", encoding="UTF-8") as file:
-    links = file.read()
+    links = []
+    for link in file:
+        links.append(link)
         
 
-page = urllib.request.urlopen("")    
+
+text = ""
+
+
+
+for link in links:
+    print(link)
+    page = urllib.request.urlopen(link)
+    html = str(page.read().decode('utf-8'))
+    soup = BeautifulSoup(html, 'lxml')
+    title = soup.title.string
+    for img in soup.find_all('img'):
+        imagem = img.attrs.get("src")
+    
+    text += f"<p>{title}</p> \n <img src={imagem} alt="">  \n"
+
+
+message = f"""<html>
+<head></head>
+<body>{text}</body>
+</html>"""
+
+
+with open("index.html", "w", encoding="UTF-8") as file:
+    file.write(message)
+    
+
+    
+    
